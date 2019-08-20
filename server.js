30578cd91a1834312ab1f26aa7248e4edc7fdfa4
iankimble <ian.kimble571@gmail.com>
2019-08-20 00:52:36 -0400

    third commit

--- server.js
+++ server.js
@@ -33,15 +33,15 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// if(process.env.NODE_ENV ==='production'){
-//     app.use(express.static('client/build'));
-// }
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+}
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
 dataRoutes.route('/addBudgetData').post((req, res) => {
