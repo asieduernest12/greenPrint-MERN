4b82c8986b847a442ab372913c6757403c2a831a
iankimble <ian.kimble571@gmail.com>
2019-08-20 07:06:18 -0400

    another

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 
 const dotenv = require('dotenv');
-// const mongoInfo = process.env.MONGO_INFO;
-// let url = mongoInfo
 
 dotenv.config();
 
